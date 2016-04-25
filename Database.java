7e61798969e24381a8eb3f3c3a6690a787f20d76
Nathan <gibson_nathaniel1@columbusstate.edu>
2016-04-24 22:29:18 -0400

    added database class

---
+++ Database.java
@@ -0,0 +1,32 @@
+import java.sql.*;   
+ 
+public class Database {  
+
+   private Connection conn;
+   private Statement stmt;
+   public Database(){
+      try{
+         conn = DriverManager.getConnection(
+               "jdbc:mysql://localhost:3306/accountdb?useSSL=false", "nathan", "password"); 
+
+         stmt = conn.createStatement();  
+      }catch(Exception e){
+         System.out.println(e);
+      }
+   }   
+
+   public boolean valid(String user, String pass) {
+   		try{
+   			String strSelect = "select * from account where username = '" + user + "\' and password = '" + pass + "'";
+	   		ResultSet rset = stmt.executeQuery(strSelect);
+	   		if (!rset.isBeforeFirst() ) {
+	   		    return false;
+	 		}
+   			return true;	
+   		}catch(Exception e){
+   			System.out.println(e);
+   			return false;
+   		}	
+   		
+   }
+}
